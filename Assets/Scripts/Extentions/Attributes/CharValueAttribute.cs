460e9a4d472116a1135749bcc21928a46015efbe
bas-boop <dereusbas461@gmail.com>
2023-12-10 02:18:08 +0100

    Added StringValueAttribute & CharValueAttribute

---
+++ Assets/Scripts/Extentions/Attributes/CharValueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Baz_geluk9.Feature_vault.Extensions
+{
+    public sealed class CharValueAttribute : Attribute
+    {
+        public char Value { get; }
+
+        public CharValueAttribute(char value) => Value = value;
+    }
+}
---
+++ Assets/Scripts/Extentions/Attributes/StringValueAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Baz_geluk9.Feature_vault.Extensions
+{
+    public sealed class StringValueAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public StringValueAttribute(string value) => Value = value;
+    }
+}
